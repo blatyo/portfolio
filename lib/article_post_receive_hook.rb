bbdc0c6e291899070d3ddf71d8ec8459000fc0e8
Allen Madsen <blatyo@gmail.com>
2011-06-01 15:00:22 -0400

    Forgot to require github api

--- lib/article_post_receive_hook.rb
+++ lib/article_post_receive_hook.rb
@@ -1,16 +1,17 @@
 require "#{Rails.root}/lib/post_receive_hook"
+require "#{Rails.root}/lib/github_api"
 
 module ArticlePostReceiveHook
   extend ActiveSupport::Concern
-  
+
   module ClassMethods
     def post_receive(push)
       super(push)
       import_articles
     end
-    
+
     private
-    
+
     def import_articles
       p push.new_files
       (push.new_files + push.updated_files).each do |file_name|
@@ -22,13 +23,13 @@ module ArticlePostReceiveHook
           article.update_attributes(:body => body, :created_at => date)
         end
       end
-      
+
       push.deleted_files.each do |file_name|
         title, date = parse_file_name(file_name)
         Article.find_by_title(title).delete
       end
     end
-    
+
     def parse_file_name(file_name)
       file_name =~ /(\d{4}-\d{2}-\d{2})\s(.+)\.md/
       [$2, $1 ? Date.parse($1) : nil] #title, date
