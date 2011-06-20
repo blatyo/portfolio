18af138f43df0daba625f01a3743da22bb7ee05b
Allen Madsen <blatyo@gmail.com>
2011-06-19 21:11:56 -0400

    specs, code, and refactoring

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
-require "#{Rails.root}/lib/article_post_receive_hook"
+require "#{Rails.root}/lib/article/post_receive_hook"
 require "#{Rails.root}/lib/markup"
 
 class Article < ActiveRecord::Base
-  include ArticlePostReceiveHook
+  include PostReceiveHook
   
   def body=(markdown)
     self[:body] = Markup.process(markdown)
---
+++ lib/article/post_receive_hook.rb
@@ -0,0 +1,49 @@
+require "#{Rails.root}/lib/github_api"
+
+class Article < ActiveRecord::Base
+  module PostReceiveHook
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def post_receive(push)
+        @push = GithubAPI::Push.new(push)
+        import_articles
+      end
+
+      private
+    
+      attr_reader :push
+
+      def import_articles
+        create_or_update_articles
+        delete_articles
+      end
+    
+      def create_or_update_articles
+        (push.added + push.modified).each do |file_name|
+          title, date, category, tags = parse_file_name(file_name)
+          if title && date
+            body = GithubAPI.get_file_with_file_name(push.owner, push.repository, 'master', file_name)
+            article = Article.find_or_initialize_by_title(title)
+            article.update_attributes(:body => body, :created_at => date)
+          end
+        end
+      end
+    
+      def delete_articles
+        push.removed.each do |file_name|
+          title, date, category, tags = parse_file_name(file_name)
+          Article.find_by_title(title).delete if title && date
+        end
+      end
+
+      def parse_file_name(file_name)
+        file_name =~ /(\d{4}-\d{2}-\d{2})\s(.+?)(\[(.+)\])?(\{(.+)\})?\.md/
+        [ $2.try(:strip),                 #title
+          $1 ? Date.parse($1) : nil,      #date
+          $4.try(:strip),                 #category
+          $6 ? $6.strip.split(',') : []]  #tags
+      end
+    end
+  end
+end
